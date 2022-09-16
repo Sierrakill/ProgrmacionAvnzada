a12ca6040222c01a9ca872a635ab5559275ba243
Sierrakill <92342394+Sierrakill@users.noreply.github.com>
2022-09-16 02:49:32 +0200

    Add files via upload

---
+++ u4/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel</title>
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+
+<body>
+    <div class="container">
+     <fieldset>
+        <form>
+            <legend>
+                Acceso al Panel
+            </legend>
+            <label>
+                Correo Electronico
+            </label>
+            <input type="email" name="">
+            <label>
+                Contraseña
+            </label>
+            <input type="password" name="">
+            <button>
+                Acceder
+            </button>
+        </form>
+     </fieldset>
+        
+    </div>
+    
+</body>
+</html>
